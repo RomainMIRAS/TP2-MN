f33091629e1a785ed43eb7987e5fc86ad815f891
RDel-Medico <redelmedico@gmail.com>
2023-02-22 15:40:02 +0100

    div functional

--- TPBLAS/src/complexe.c
+++ TPBLAS/src/complexe.c
@@ -39,7 +39,7 @@ complexe_double_t mult_complexe_double (const complexe_double_t c1, const comple
   r.real = c1.real * c2.real - c1.imaginary * c2.imaginary;
 
   r.imaginary = c1.imaginary * c2.real + c1.real * c2.imaginary;
-  
+
   return r ;
 }
   
@@ -53,28 +53,29 @@ complexe_float_t div_complexe_float (const complexe_float_t c1, const complexe_f
   inverseBot.real = c2.real;
 
   complexe_float_t top = mult_complexe_float(c1, inverseBot);
-  complexe_float_t bot = mult_complexe_float(c2, inverseBot);
+  float bot = mult_complexe_float(c2, inverseBot).real;
 
-  r.real = top.real/bot.real;
-  r.imaginary = bot.imaginary/bot.real;
+  r.real = top.real/bot;
+  r.imaginary = top.imaginary/bot;
 
   
   return r ;
 }
 
+//14 opérations
 complexe_double_t div_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
 {
   complexe_double_t r ;
 
   complexe_double_t inverseBot;
-  inverseBot.imaginary = -c2.imaginary;
   inverseBot.real = c2.real;
+  inverseBot.imaginary = -c2.imaginary;
 
   complexe_double_t top = mult_complexe_double(c1, inverseBot);
-  complexe_double_t bot = mult_complexe_double(c2, inverseBot);
+  float bot = mult_complexe_double(c2, inverseBot).real;
 
-  r.real = top.real/bot.real;
-  r.imaginary = bot.imaginary/bot.real;
+  r.real = top.real/bot;
+  r.imaginary = top.imaginary/bot;
   
   return r ;
 }
