02dba739ebba57c927bf36be8307ec3222b4e9fe
RDel-Medico <redelmedico@gmail.com>
2023-02-22 14:42:22 +0100

    mult complex float

--- TPBLAS/src/complexe.c
+++ TPBLAS/src/complexe.c
@@ -20,9 +20,14 @@ complexe_double_t add_complexe_double (const complexe_double_t c1, const complex
   return r ;
 }
 
+//6 opérations
 complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
 {
   complexe_float_t r ;
+
+  r.real = c1.real * c2.real - c1.imaginary * c2.imaginary;
+
+  r.imaginary = c1.imaginary * c2.real + c1.real * c2.imaginary;
   
   return r ;
 }
