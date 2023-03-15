8acc89846badc6f4d946313e3db983ff3526701e
RDel-Medico <redelmedico@gmail.com>
2023-03-15 15:16:01 +0100

    ca marche

--- TPBLAS/src/swap.c
+++ TPBLAS/src/swap.c
@@ -35,14 +35,27 @@ void mncblas_dswap(const int N, double *X, const int incX,
 }
 
 void mncblas_cswap(const int N, void *X, const int incX, 
-		                    void *Y, const int incY)
+		                    void *Y, const int incY) // float // Réel first iM second
 {
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
+  register float save;
   
+  for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
+    {
+      save = ((float*)Y )[j] ;
+      ((float*)Y )[j] = ((float*)X )[i] ;
+      ((float*)X )[i]  = save ;
+
+      save = ((float*)Y )[j] ;
+      ((float*)Y )[j+1] = ((float*)X )[i+1] ;
+      ((float*)X )[i+1] = save ;
+    }
   return ;
 }
 
 void mncblas_zswap(const int N, void *X, const int incX, 
-		                    void *Y, const int incY)
+		                    void *Y, const int incY) // double
 {
 
   return ;
